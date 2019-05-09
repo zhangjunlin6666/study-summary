decb7e5b91140664440acef2399f90ebb0267144
jackson <zhangjunlin@tengyue360.com>
2019-05-09 22:57:06 +0800

    内容更新

--- publish.sh
+++ publish.sh
@@ -1,10 +1,3 @@
-# 切换到该分支后，执行该脚本，首先删除除了.sh、.gitignore文件
-rm -fr `ls ./ | egrep -v '(publish.sh|.gitignore)'`
-# 将master的代码合并到当前分支
-git fetch all
-git reset --hard origin/gh-pages
-git pull origin master
-# 删除当前目录下以part开头、以.md为结尾的文件
 rm -fr *.md part*/*.md book.json 
 # 判断是否有_book文件
 if [ -d "_book" ]; then
