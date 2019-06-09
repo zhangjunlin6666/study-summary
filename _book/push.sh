70bf4d9849ffb7e95f3f55023f3639aacc847b25
jackson <zhangjunlin@tengyue360.com>
2019-06-09 23:16:53 +0800

    内容更新

--- _book/push.sh
+++ _book/push.sh
@@ -2,7 +2,7 @@
 #判断当前分支是否是master，如果是就执行判断后面的脚本
 d=`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`
 echo $d
-if [ $d == "gh-pages" ];then
+if [ $d != "master" ];then
     echo '当前分支不是master分支不能执行该脚本';
     exit 0;
 fi
