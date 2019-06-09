89dcd51449b7d6de942872be18929a90b3339fc1
jackson <zhangjunlin@tengyue360.com>
2019-06-10 00:22:04 +0800

    内容更新

--- publish.sh
+++ publish.sh
@@ -7,12 +7,12 @@ if [ $d != "gh-pages" ];then
 fi
 # 先拉取master的代码
 git pull origin master
-# 删除根目录、classification下的md文件以及book.json和sh脚本文件下的push脚本 =============
-rm -fr ./*.md ./classification/**/*.md ./book.json ./push.sh
-# 将目录_book下的文件复制到当前目录下 ================
-cp -r _book/* .
-# 删除_book文件夹 ============
-rm -rf _book
+# # 删除根目录、classification下的md文件以及book.json和sh脚本文件下的push脚本 =============
+# rm -fr ./*.md ./classification/**/*.md ./book.json ./push.sh
+# # 将目录_book下的文件复制到当前目录下 ================
+# cp -r _book/* .
+# # 删除_book文件夹 ============
+# rm -rf _book
 # 提交至github仓库
 git add .
 git commit -m '内容更新'
