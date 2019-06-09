62fafde04aa09c45dd7eed4d07412b65fcafa40c
jackson <zhangjunlin@tengyue360.com>
2019-06-09 22:52:53 +0800

    内容更新

---
+++ _book/publish.sh
@@ -0,0 +1,13 @@
+rm -fr *.md part*/*.md book.json push.sh
+# 判断是否有_book文件
+if [ -d "_book" ]; then
+    echo '删除啦'
+    # 将目录_book下的文件复制到当前目录下
+    cp -r _book/* .
+    # 删除_book文件夹
+    rm -rf _book
+fi
+# 提交至github仓库
+git add .
+git commit -m '内容更新'
+git push origin gh-pages
