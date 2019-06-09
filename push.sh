6aec5d888bc9933bed42be7366951a6bdb86c5d2
jackson <zhangjunlin@tengyue360.com>
2019-06-09 23:14:57 +0800

    内容更新

--- push.sh
+++ push.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
+#判断当前分支是否是master，如果是就执行判断后面的脚本
 d=`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`
-echo ${d}
+echo $d
+if [ $d == "gh-pages" ];then
+    echo '当前分支不是master分支不能执行该脚本';
+    exit 0;
+fi
 # 先通过gitbook build构建html，并提交
 gitbook build
 git add .
