51b2fefc289bcbc7eca798ac0901adaf6ef70a50
jackson <zhangjunlin@tengyue360.com>
2020-01-04 18:14:26 +0800

    feat：增加travis自动部署

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
  # @Author: jackson
  # @Date: 2020-01-03 11:49:25
  # @LastEditors  : jackson
- # @LastEditTime : 2020-01-04 17:28:42
+ # @LastEditTime : 2020-01-04 18:10:18
  ###
 #!/usr/bin/env sh
 
@@ -26,6 +26,11 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/zhangjunlin6666/study-summary.git master:gh-pages
+#git push -f https://github.com/zhangjunlin6666/study-summary.git master:gh-pages
+
+# 如果使用 travis 持续集成 
+# travis ci平台 https://www.travis-ci.org/zhangjunlin6666/study-summary/settings
+# vuepress从搭建到部署 https://www.jianshu.com/p/a7435b8bc8bc
+git push -f https://${access_token}@github.com/zhangjunlin6666/study-summary.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,11 @@
  * @Author: jackson
  * @Date: 2020-01-02 17:47:15
  * @LastEditors  : jackson
- * @LastEditTime : 2020-01-03 11:47:13
+ * @LastEditTime : 2020-01-04 18:13:48
  */
 module.exports = {
     base: '/study-summary/',
-    title: '心尘的个人技术博客',
+    title: '心尘的技术博客',
     description: 'Just playing around',
     head: [
         ['link', { rel: 'icon', type: "image/x-icon", href: `/study-summary/img/logo.jpg`}],
